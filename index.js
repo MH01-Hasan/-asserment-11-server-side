fd9652d8b22d36c8a33da99ec03faed955272a09
MH01-Hasan <mehediph2021@gmail.com>
2021-11-01 01:17:39 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+ require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const cors = require('cors')
@@ -17,8 +18,7 @@ app.use(express.json());
 
 
 //connect to data base and node server code start//
-const uri = "mongodb+srv://mydbUser1:cLNLMdZgklQCLXqe@cluster0.bs9pl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bs9pl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 //connect to data base and node server code end//
 
